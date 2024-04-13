b9719b644f9a07b8a7a40ef8180dcf67142fdac9
iamabutalha <maabutalhakhalil@gmail.com>
2024-04-13 09:35:03 +0500

    Error fixed

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 
 class AuctionListingForm(forms.Form):
@@ -7,5 +8,98 @@ class AuctionListingForm(forms.Form):
         widget=forms.TextInput(attrs={
             'class': 'from-control from-group',
             'placeholder':'Give it a Title '
+        }
+        )
+    )
+
+    description = forms.CharField(
+        label='Description',
+        required=True,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'Tell more about product',
+            'row': '3'
+        }
+        )
+    )
+
+    price = forms.DecimalField(
+        label='Price',
+        required=True,
+        initial=0.00,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'Estimated Price (optional)',
+            'min':'0.01',
+            'max':'99999999.99',
+            'step': '0.1'
+
+        }
+        )
+
+    )
+
+   
+    starting_bid = forms.DecimalField(
+        label='Starting bid',
+        required=True,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'strating Bid',
+            'min':'0.01',
+            'max':'999999999.9',
+            'step': '0.1'   
+        }
+        )
+       
+    )
+
+    category = forms.CharField(
+        label='Category',
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control form-group',
+            'autocomplete': 'on',
+            'placeholder': 'Category (optional)'
         })
     )
+
+    image_url = forms.URLField(
+        label='Image URL',
+        required=False,
+        initial='https://images.app.goo.gl/h7sWyqSwujg2qvzN9',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control form-group',
+            'placeholder': 'Image URL (optional)',
+        })
+    )
+
+    def clean_stsrting_bid(self):
+        amount = float(self.cleaned_data.get('starting_bid'))
+        if isinstance(amount,float) and amount > 0:
+            return amount
+        print(amount)
+        raise forms.ValidationError('Should be a Number Larger Than 0')
+    
+    def clean_category(self):
+        category = self.cleaned_data.get('category')
+        return category.lower()
+    
+
+class CommentForm(forms.Form):
+    text = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control-md lead form-group',
+            'row': '3',
+            'cols':'100'
+        }
+        )
+    )
+
+    def clean_comment(self):
+        text = self.cleaned_data.get('text')
+        if len(text) > 0:
+            return text
+        return self.errors
--- auctions/views.py
+++ auctions/views.py
@@ -9,11 +9,13 @@ from django.contrib.auth.decorators import login_required
 from .models import User
 
 from .models import User, AuctionListing, Bid, Comment
-from .forms import AuctionListingForm,
+from .forms import AuctionListingForm, CommentForm
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html",{
+        "listing": AuctionListing.objects.all().order_by('-created_at')
+    })
 
 
 def login_view(request):
@@ -34,6 +36,7 @@ def login_view(request):
             })
     else:
         return render(request, "auctions/login.html")
+    
 
 
 def logout_view(request):
@@ -66,3 +69,127 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required(login_url='auctions/login.html')
+def create(request):
+    return render(request, 'auctions/login.html',{
+        'form' : AuctionListingForm
+    })
+
+@login_required(login_url='auctions/login.html')
+def insert(request):
+    form = AuctionListingForm(request.POST)
+    if form.is_valid:
+        auction = AuctionListing(user=request.user, **form.cleaned_date)
+        if not auction.image_url:
+            auction.image_url = 'https://images.app.goo.gl/h7sWyqSwujg2qvzN9'
+        auction.save()
+
+        starting_bid = auction.starting_bid
+
+        bid = Bid(amount=starting_bid, user=request.user, auction=auction)
+        bid.save()
+        print("auction" + auction.image_url)
+
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'auction/create/html',{
+            'form': form,
+            'error': form.errors
+        })
+    
+
+def listing(request, id):
+    current = AuctionListing.objects.get(pk=id)
+    bid = get_list_or_404(Bid, auction=current)
+    comments = Comment.objects.filter(auction=current)
+    print("here:" + AuctionListing.objects.get(pk=id).image_url)
+    return render(request, 'auctions/listing.html',{
+        'auction': current,
+        'user': request.user,
+        'bid': bid,
+        'comments': comments,
+        'comment_form':CommentForm()
+    })
+
+@login_required(login_url='auctions/login.html')
+def update_bid(request, id):
+    amount = request.POST['bid']
+    if amount:
+        amount = float(amount)
+        auction = get_list_or_404(AuctionListing, id=id)
+        if amount > get_list_or_404(Bid, id=id).amount:
+            bid = get_list_or_404(Bid, id=id)
+            bid.user, bid.amount = request.user, amount 
+            bid.save()
+            auction.bid_counter +=1
+            auction.save()
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            raise ValidationError('Bid Must be greater than the current bid value')
+    else:
+        raise ValidationError('Bid Must be greater than current Bid value')
+    
+@login_required(login_url='auctions/login.html')
+def close_bid(request, id):
+    auction = get_list_or_404(AuctionListing,id=id)
+    auction.active, auction.winner = False, request.user.username
+    auction.save()
+    return HttpResponseRedirect(reverse('index'))
+
+@login_required(login_url='auctions/login.html')
+def watchlist(request):
+    return render(request, 'auctions/watchlist.html',{
+        "watchlist": request.user.watchlist.all()
+    })
+
+@login_required(login_url='auctions/login.html')
+def watch(request, id):
+    auction = get_list_or_404(AuctionListing,id=id)
+    request.user.watchlist.add(auction)
+    request.user.watchlist_counter +=1
+    request.user.save()
+    return HttpResponseRedirect(reverse('index'))
+
+@login_required(login_url='auctions/login.html')
+def unwatch(request, id):
+    auction = get_list_or_404(AuctionListing,id=id)
+    request.user.watchlist.remove(auction)
+    request.user.watchlist_counter -=1
+    request.user.save()
+
+    if '/unwatch/' in request.path:
+        return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('wishlist'))
+
+
+def categories(request):
+    return render(request, 'auctions/categories.html')
+
+
+def filter(request):
+    q = request.GET['category'].lower()
+    return render(request, 'auctions/category.html',{
+        'listing' : AuctionListing.objects.filter(category=q)
+    })
+
+def add_comment(request, id):
+    anonymous = User.first_name
+    if request.user is not anonymous:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            f = form.cleaned_data
+            comment = Comment(
+                user = request.user,
+                auction = get_list_or_404(AuctionListing, id=id),
+                **f
+            )
+            comment.save()
+            return HttpResponseRedirect(reverse('listing', kwargs={
+                'id':id
+            }))
+        else:
+            return render(request, 'auction/login.html',{
+                'message':'Must be logged in to be able to comment'
+            })
+        
